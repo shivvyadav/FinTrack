342e4c2864ff6057bf331de5f6f284f019d71c7d
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-26 22:46:24 +0545

    add tailwind cofiguration for the project

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+// tailwind.config.js (in root directory)
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
