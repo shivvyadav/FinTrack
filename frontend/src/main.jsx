658ffab4588bc42e0b5c92dd21a9e7fcb2cf791f
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-28 23:42:42 +0545

    add toaster in main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <div>
+    <Toaster
+      position="top-center"
+      reverseOrder={false}
+      toastOptions={{
+        success: {
+          style: {
+            background: "white",
+            color: "green",
+          },
+        },
+        error: {
+          style: {
+            background: "white",
+            color: "red",
+          },
+        },
+      }}
+    />
     <App />
-  </StrictMode>,
-)
+  </div>,
+);
