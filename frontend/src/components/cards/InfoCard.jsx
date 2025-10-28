56b83a6062a14065419f58ebbb3a6644d3d9c113
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-28 23:06:12 +0545

    add infoCard component

---
+++ frontend/src/components/cards/InfoCard.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const InfoCard = ({ icon, label, value, color }) => {
+  return (
+    <div className="bg-foreground text-primary border-border flex gap-6 rounded-2xl border p-6 shadow-md transition-colors duration-300">
+      <div
+        className={`${color} flex size-14 items-center justify-center rounded-full text-[26px]`}
+      >
+        {icon}
+      </div>
+      <div>
+        <h6 className="text-secondary mb-1 text-sm">{label}</h6>
+        <span className="text-[22px]">$ {value}</span>
+      </div>
+    </div>
+  );
+};
+
+export default InfoCard;
