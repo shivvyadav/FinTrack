438f4ba0c4b16c420e26ccaa894da6223ec64015
Shiv Yadav <shivyadav0014@gmail.com>
2025-10-29 22:53:31 +0545

    add helper functions for form validations

---
+++ frontend/src/helpers/validation.js
@@ -0,0 +1,8 @@
+//email validation
+export const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
+
+//password validation
+export const validatePassword = (password) => password.length >= 8;
