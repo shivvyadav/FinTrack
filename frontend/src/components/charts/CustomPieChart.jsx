6d670d66c5e5fdbe1c26979a4b7f55b87b6688eb
Shiv Yadav <shivyadav0014@gmail.com>
2025-11-20 08:39:32 +0545

    add custom pie-chart

---
+++ frontend/src/components/charts/CustomPieChart.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import CustomTooltip from "./CustomTooltip";
+import CustonLegend from "./CustonLegend";
+
+import {
+  PieChart,
+  Pie,
+  Cell,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+
+const CustomPieChart = ({
+  data,
+  label,
+  totalAmount,
+  colors,
+  showTextAnchor,
+}) => {
+  return (
+    <ResponsiveContainer width="100%" height={380}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="amount"
+          nameKey="name"
+          cx="50%"
+          cy="50%"
+          outerRadius={130}
+          innerRadius={100}
+          labelLine={false}
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+          ))}
+        </Pie>
+        <Tooltip content={<CustomTooltip />} />
+        <Legend content={CustonLegend} />
+
+        {showTextAnchor && (
+          <>
+            <text
+              x="50%"
+              y="50%"
+              dy={-25}
+              textAnchor="middle"
+              dominantBaseline="middle"
+              fill="#666"
+              fontSize="14px"
+            >
+              {label}){" "}
+            </text>
+            <text
+              x="50%"
+              y="50%"
+              dy={8}
+              textAnchor="middle"
+              dominantBaseline="middle"
+              fill="#333"
+              fontSize="24px"
+              fontWeight="semi-bold"
+            >
+              {totalAmount})
+            </text>
+          </>
+        )}
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default CustomPieChart;
