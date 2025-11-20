bf72c400d4af8c5dec622abde05fe6a25495f40e
Shiv Yadav <shivyadav0014@gmail.com>
2025-11-20 08:41:30 +0545

    add finance overview section

---
+++ frontend/src/components/dashboard/FinanceOverview.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import CustomPieChart from "../charts/CustomPieChart";
+
+const COLORS = ["#875CF5", "#FA2C37", "#FF6900"];
+const FinanceOverview = ({ totalExpense, totalIncome, totalBalance }) => {
+  const balanceData = [
+    { name: "Total Balance", amount: totalBalance },
+    { name: "Total Expense", amount: totalExpense },
+    { name: "Total Income", amount: totalIncome },
+  ];
+  return (
+    <div className="card">
+      <div className="flex items-center justify-between">
+        <h5 className="text-lg">Finance Overview</h5>
+      </div>
+
+      <CustomPieChart
+        data={balanceData}
+        label="Total Balance "
+        totalAmount={`$${totalBalance}`}
+        colors={COLORS}
+        showTextAnchor
+      />
+    </div>
+  );
+};
+
+export default FinanceOverview;
