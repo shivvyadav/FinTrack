5844db6823e1cd122004a431fd7982d61d785cf3
Shiv Yadav <shivyadav0014@gmail.com>
2025-11-20 23:58:12 +0545

    add expense list

---
+++ frontend/src/components/expense/ExpenseList.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import TransactionInfoCard from "../cards/TransactionInfoCard";
+import moment from "moment";
+import { Download } from "lucide-react";
+
+const ExpenseList = ({ transactions, onDelete, onDownload }) => {
+  return (
+    <div className="card">
+      <div className="flex items-center justify-between">
+        <h5 className="text-lg font-medium">Expenses</h5>
+        <button onClick={onDownload} className="card-btn">
+          <Download className="h-4 w-4" />
+        </button>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2">
+        {transactions?.map((expense) => (
+          <TransactionInfoCard
+            key={expense._id}
+            title={expense.category}
+            amount={expense.amount}
+            date={moment(expense.date).format("Do MMM, YYYY")}
+            icon={expense.icon}
+            type="expense"
+            hideDeleteBtn={false}
+            onDelete={() => onDelete(expense._id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseList;
