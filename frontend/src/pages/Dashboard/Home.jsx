d692d29b4cc68c1474aaca37c0750c34c7210338
Shiv Yadav <shivyadav0014@gmail.com>
2025-11-23 23:00:34 +0545

    update home page

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -1,13 +1,19 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
+import { UserContext } from "../../context/UserContext";
 import DashboardLayout from "../../components/DashboardLayout";
 import InfoCard from "../../components/cards/InfoCard";
+import RecentTransactions from "../../components/dashboard/RecentTransactions";
+import ExpenseTransactions from "../../components/dashboard/ExpenseTransactions";
+import Last60DaysExpenses from "../../components/dashboard/Last60DaysExpenses";
+import FinanceOverview from "../../components/dashboard/FinanceOverview";
+import RecentIncomeWithChart from "../../components/dashboard/RecentIncomeWithChart";
+import RecentIncome from "../../components/dashboard/RecentIncome";
 import { useNavigate } from "react-router-dom";
 import { HandCoins, WalletMinimal, CreditCard } from "lucide-react";
 
 const Home = () => {
   const navigate = useNavigate();
-
   const [dashboardData, setDashboardData] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -36,7 +42,7 @@ const Home = () => {
 
   return (
     <DashboardLayout activeMenu="Dashboard">
-      <div className="mx-auto my-5">
+      <div className="pt-20 pr-1 pb-4 sm:pr-4">
         <div className="grid grid-cols-1 gap-6 md:grid-cols-3">
           <InfoCard
             icon={<CreditCard />}
@@ -56,6 +62,43 @@ const Home = () => {
             value={dashboardData?.totalExpense || 0}
             color="bg-red-400"
           />
+        </div>
+
+        <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-2">
+          <RecentTransactions
+            transactions={dashboardData?.recentTransactions || []}
+          />
+          <FinanceOverview
+            totalBalance={dashboardData?.totalBalance || 0}
+            totalIncome={dashboardData?.totalIncome || 0}
+            totalExpense={dashboardData?.totalExpense || 0}
+          />
+          {dashboardData?.last60daysExpenseTransaction.length > 0 && (
+            <ExpenseTransactions
+              transactions={dashboardData?.last60daysExpenseTransaction || []}
+              onSeeMore={() => navigate("/expense")}
+            />
+          )}
+          {dashboardData?.last60daysExpenseTransaction.length > 0 && (
+            <Last60DaysExpenses
+              data={dashboardData?.last60daysExpenseTransaction || []}
+            />
+          )}
+
+          {dashboardData?.last60daysIncomeTransaction?.length > 0 && (
+            <RecentIncomeWithChart
+              data={
+                dashboardData?.last60daysIncomeTransaction?.slice(0, 4) || []
+              }
+              totalIncome={dashboardData?.last60daysIncomeBalance || 0}
+            />
+          )}
+          {dashboardData?.last60daysIncomeTransaction?.length > 0 && (
+            <RecentIncome
+              transactions={dashboardData?.last60daysIncomeTransaction || []}
+              onSeeMore={() => navigate("/income")}
+            />
+          )}
         </div>
       </div>
     </DashboardLayout>
