d67a54b3baef006c8afb33078ba47841b28cbc2d
Shiv Yadav <shivyadav0014@gmail.com>
2025-11-27 23:05:09 +0545

    update dashboard layout

--- frontend/src/components/DashboardLayout.jsx
+++ frontend/src/components/DashboardLayout.jsx
@@ -5,17 +5,19 @@ import Sidebar from "./Sidebar";
 
 const DashboardLayout = ({ activeMenu, children }) => {
   const { user } = useContext(UserContext);
+
   return (
-    <div className="bg-background transition-colors duration-300">
+    <div className="bg-background h-screen overflow-hidden transition-colors duration-300">
       <Navbar activeMenu={activeMenu} />
 
       {user && (
-        <div className="flex">
-          <div className="max-[1080px]:hidden">
+        <div className="flex overflow-hidden">
+          <div className="h-[calc(100vh-61px)] max-[1080px]:hidden">
             <Sidebar activeMenu={activeMenu} />
           </div>
-
-          <div className="mx-5 grow">{children}</div>
+          <div className="h-screen grow overflow-y-auto pl-2 sm:pl-4 xl:pl-69">
+            {children}
+          </div>
         </div>
       )}
     </div>
@@ -23,3 +25,29 @@ const DashboardLayout = ({ activeMenu, children }) => {
 };
 
 export default DashboardLayout;
+
+// import React, { useContext } from "react";
+// import { UserContext } from "../context/UserContext";
+// import Navbar from "./Navbar";
+// import Sidebar from "./Sidebar";
+
+// const DashboardLayout = ({ activeMenu, children }) => {
+//   const { user } = useContext(UserContext);
+//   return (
+//     <div className="transition-colors duration-300 bg-background">
+//       <Navbar activeMenu={activeMenu} />
+
+//       {user && (
+//         <div className="flex">
+//           <div className="max-[1080px]:hidden">
+//             <Sidebar activeMenu={activeMenu} />
+//           </div>
+
+//           <div className="mx-5 grow">{children}</div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default DashboardLayout;
