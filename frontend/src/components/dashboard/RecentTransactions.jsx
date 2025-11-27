d4d6da2cebce1840a0f10047c86746f8c43a9581
Shiv Yadav <shivyadav0014@gmail.com>
2025-11-27 23:03:13 +0545

    update recent transaction

--- frontend/src/components/dashboard/RecentTransactions.jsx
+++ frontend/src/components/dashboard/RecentTransactions.jsx
@@ -3,19 +3,15 @@ import moment from "moment";
 import TransactionInfoCard from "../cards/TransactionInfoCard";
 import { ArrowRight } from "lucide-react";
 
-const RecentTransactions = ({ transactions, onSeeMore }) => {
+const RecentTransactions = ({ transactions }) => {
   return (
     <div className="card">
       <div className="flex items-center justify-between">
-        <h5 className="text-lg">Recent Transactions</h5>
-
-        <button className="card-btn" onClick={onSeeMore}>
-          See All <ArrowRight size={16} />
-        </button>
+        <h5 className="text-lg font-medium">Recent Transactions</h5>
       </div>
 
       <div className="mt-6">
-        {transactions.length > 0 &&
+        {transactions.length > 0 ? (
           transactions
             .slice(0, 5)
             .map((tx) => (
@@ -23,12 +19,15 @@ const RecentTransactions = ({ transactions, onSeeMore }) => {
                 key={tx._id}
                 title={tx.type == "expense" ? tx.category : tx.source}
                 icon={tx.icon}
-                date={moment(tx.date).format("DD MMM, YYYY")}
+                date={moment(tx.date).format("Do MMM, YYYY")}
                 amount={tx.amount}
                 type={tx.type}
                 hideDeleteBtn
               ></TransactionInfoCard>
-            ))}
+            ))
+        ) : (
+          <p className="text-sm text-gray-400">No transactions found</p>
+        )}
       </div>
     </div>
   );
