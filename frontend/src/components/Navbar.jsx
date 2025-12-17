9a5b8fcc9543dca36a3d6389db85e5b0127d59e6
Shiv Yadav <shivyadav0014@gmail.com>
2025-12-17 19:41:41 +0545

    add icon as logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,18 +10,21 @@ const Navbar = ({ activeMenu }) => {
       {/* Fixed Navbar */}
       <div className="bg-foreground border-border fixed top-0 left-0 z-40 flex w-full gap-5 border-b px-4 py-4 backdrop-blur-[2px] transition-colors duration-300 sm:px-5 md:px-6">
         <button
-          className="text-primary block xl:hidden"
+          className="block text-primary xl:hidden"
           onClick={() => setOpenSideMenu(!openSideMenu)}
         >
           {openSideMenu ? (
-            <X className="h-6 w-6 md:size-7" />
+            <X className="w-6 h-6 md:size-7" />
           ) : (
-            <Menu className="h-6 w-6 md:size-7" />
+            <Menu className="w-6 h-6 md:size-7" />
           )}
         </button>
 
-        <h2 className="text-primary bg-secondary text-xl font-semibold">
-          FinTrack
+        <h2 className="ml-2 space-x-2 text-xl font-semibold text-primary bg-secondary">
+          <span>FinTrack</span>
+          <span>
+            <img src="./fav.png" alt="" className="inline-block h-8 pb-1" />
+          </span>
         </h2>
       </div>
 
