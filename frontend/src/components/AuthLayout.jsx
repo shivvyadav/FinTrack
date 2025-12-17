714a97bb27ead4d643f9a95b1da25a0bdc67b38c
Shiv Yadav <shivyadav0014@gmail.com>
2025-12-17 19:41:12 +0545

    update title

--- frontend/src/components/AuthLayout.jsx
+++ frontend/src/components/AuthLayout.jsx
@@ -1,27 +1,28 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import SpinnerCustom from "./SpinnerCustom";
 
 const AuthLayout = ({ title, children }) => {
   const { pathname } = useLocation();
   const isLogin = pathname === "/login";
 
   return (
-    <div className="bg-background text-primary flex min-h-screen transition-colors duration-300">
+    <div className="flex min-h-screen transition-colors duration-300 bg-background text-primary">
       {/* Left section - form */}
-      <div className="relative flex w-full flex-col px-6 pt-8 sm:px-12 md:w-1/2 lg:px-20">
-        <h1 className="text-primary text-2xl font-semibold">FinTrack</h1>
+      <div className="relative flex flex-col w-full px-6 pt-8 sm:px-12 md:w-1/2 lg:px-20">
+        <h1 className="text-2xl font-semibold text-primary">FinTrack</h1>
         <div>
           <h2 className="mt-4 mb-2 text-xl font-semibold">
             {isLogin ? "Welcome to FinTrack" : "Create your FinTrack account"}
           </h2>
-          <p className="text-secondary mb-6">
+          <p className="mb-6 text-secondary">
             {isLogin
               ? "Start your experience with FinTrack by signing in."
               : "Join FinTrack to manage your expenses smarter."}
           </p>
 
           {/* Toggle Tabs */}
-          <div className="border-border mb-8 flex space-x-2 rounded-xl border p-1">
+          <div className="flex p-1 mb-8 space-x-2 border border-border rounded-xl">
             <Link
               to="/login"
               className={`w-1/2 rounded-lg py-2 text-center ${
@@ -41,22 +42,22 @@ const AuthLayout = ({ title, children }) => {
           </div>
 
           {/* Form Section */}
-          <div className="bg-foreground border-border rounded-2xl border p-6 shadow-sm">
+          <div className="p-6 border shadow-sm bg-foreground border-border rounded-2xl">
             {children}
           </div>
         </div>
       </div>
 
       {/* Right Section - Hero Image */}
-      <div className="bg-foreground border-border hidden w-1/2 flex-col items-center justify-center border-l md:flex">
+      <div className="flex-col items-center justify-center hidden w-1/2 border-l bg-foreground border-border md:flex">
         <img src="./finn.png" alt="FinTrack" className="" />
         <div className="px-10 text-center">
-          <div className="bg-background rounded-2xl p-10 shadow-sm">
-            <h3 className="text-primary mb-3 text-xl font-semibold">
+          <div className="p-10 shadow-sm bg-background rounded-2xl">
+            <h3 className="mb-3 text-xl font-semibold text-primary">
               A Unified Hub for Smarter Financial Decision-Making
             </h3>
-            <p className="text-secondary text-sm leading-relaxed">
-              Kezak empowers you with a unified financial command center —
+            <p className="text-sm leading-relaxed text-secondary">
+              FinTrack empowers you with a unified financial command center —
               delivering deep insights and a 360° view of your entire economic
               world.
             </p>
